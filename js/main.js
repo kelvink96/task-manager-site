6550f7cc09ffd2b03acb44aea6a1a5eecd645662
Kelvin Kiptum Kiprop <kelvin.kiprop96@gmail.com>
2022-01-17 14:59:37 +0300

    # Index - task & sub-tasks cards design

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+$.fn.toggleHTML = function(t1, t2){
+  if(this.html() == t1){
+    this.html(t2);
+  }else{
+    this.html(t1);
+  }
+  return this;
+};
+
 $(document).ready(function(){
   $('.collapse-toggle').on('click', function() {
     $(".sidebar").toggleClass('sidebar--Collapse');
@@ -5,4 +14,12 @@ $(document).ready(function(){
   });
   $('[data-bs-toggle="tooltip"]').tooltip();
   $('[data-second-bs-toggle="tooltip"]').tooltip();
+
+
+  $('.btn-sub-task-collapse').on('click', (evt) => {
+    const _this = $(evt.target).parent();
+
+    _this.find("span").toggleHTML("show more", "show less");
+    _this.find("i").toggleClass("fa-expand-arrows-alt");
+  })
 });
